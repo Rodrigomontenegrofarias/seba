6b4168a4dc41510d29a24cc2462bee6bfbd8dc0c
Bastian Peso <beps1@hotmail.com>
2020-10-06 17:52:27 -0300

    formatting code

--- src/api/centros/centro.controller.js
+++ src/api/centros/centro.controller.js
@@ -98,7 +98,7 @@ module.exports = {
             next(exception);
         }
     },
-    
+
 
 
 
--- src/api/centros/centro.model.js
+++ src/api/centros/centro.model.js
@@ -2,10 +2,10 @@
 const mongoose = require('mongoose');
 
 const centroSchema = new mongoose.Schema({
-   name: {
-       type: String,
-       required: true
-   }
+    name: {
+        type: String,
+        required: true
+    }
 });
 
 const Centro = mongoose.model("Centro", centroSchema);
