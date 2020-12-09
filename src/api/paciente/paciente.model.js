db2bb92460304097a04ebe627b8a8905df794de5
DevMan-VR <manujamming@gmail.com>
2020-12-09 20:33:17 -0300

    fix modelo paciente

--- src/api/paciente/paciente.model.js
+++ src/api/paciente/paciente.model.js
@@ -2,21 +2,14 @@
 const mongoose = require('mongoose');
 
 const pacienteSchema = new mongoose.Schema({
-    nombre: {
-        type: String,
-        required: true
-    },
     peso: {
         type: Number,
-        required: true
     },
     edad: {
         type: Number,
-        required: true
     },
     altura: {
         type: Number,
-        required: true
     },
     name: {
         type: String,
